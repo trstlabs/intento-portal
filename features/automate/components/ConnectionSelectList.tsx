3c1bf1a12600604759d4ac547621b8a017d14217
p_daniel <daniel@trstlabs.xyz>
2023-05-31 08:16:47 +0200

    gitignore

--- features/automate/components/ConnectionSelectList.tsx
+++ features/automate/components/ConnectionSelectList.tsx
@@ -106,7 +106,7 @@ export const ConnectionSelectList = ({
 }
 
 const StyledButtonForRow = styled(ButtonForWrapper, {
-  display: 'flex',
+  //display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   padding: '$2 $4 !important',
