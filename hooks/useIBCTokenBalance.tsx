a1660e5d826dd6503883af0e421b908a8b9e1db4
danieljdd <danieljdijkstra@gmail.com>
2025-02-24 10:42:50 +0100

    improve query refresh

--- hooks/useIBCTokenBalance.tsx
+++ hooks/useIBCTokenBalance.tsx
@@ -4,33 +4,37 @@ import { useRecoilValue } from 'recoil'
 import { convertMicroDenomToDenom } from 'util/conversion'
 
 import { WalletStatusType, ibcWalletState } from '../state/atoms/walletAtoms'
-import { DEFAULT_REFETCH_INTERVAL } from '../util/constants'
 import { useIBCAssetInfo } from './useIBCAssetInfo'
+
 
 export const useIBCTokenBalance = () => {
   const { status, tokenSymbol: symbol, address } = useRecoilValue(ibcWalletState)
-
   const ibcAsset = useIBCAssetInfo(symbol)
 
   const { data: balance = 0, isLoading } = useQuery(
     `ibcTokenBalance/${symbol}/${address}`,
     async () => {
+      if (!symbol || !address) {
+        // Return default value if symbol or address is not available
+        return 0
+      }
+
       const { denom, decimals } = ibcAsset
-      const chainClient = await SigningStargateClient.connect(
-        ibcAsset.rpc
-      )
+      const chainClient = await SigningStargateClient.connect(ibcAsset.rpc)
       const coin = await chainClient.getBalance(address, denom)
 
       const amount = coin ? Number(coin.amount) : 0
       return convertMicroDenomToDenom(amount, decimals)
     },
     {
-      enabled: Boolean(status === WalletStatusType.connected && address && symbol && ibcAsset && ibcAsset.denom && ibcAsset.rpc),
-      refetchOnMount: 'always',
-      refetchInterval: DEFAULT_REFETCH_INTERVAL,
-      refetchIntervalInBackground: true,
+      enabled: Boolean(status === WalletStatusType.connected && address && symbol && ibcAsset && ibcAsset.denom && ibcAsset.rpc), // Ensures query is only enabled when all conditions are met
+      refetchOnMount: 'always', // Refetch when the component mounts
+      refetchInterval: 30000,    // Refetch every 30 seconds
+      staleTime: 30000,          // Cache expires after 30 seconds
+      cacheTime: 300000,         // Cache data for 5 minutes
     }
   )
+
 
   return { balance, isLoading }
 }
