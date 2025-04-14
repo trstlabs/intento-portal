bc92f71023a6bf9796ca038e73b9f534c61bc670
danieljdd <danieljdijkstra@gmail.com>
2025-04-14 14:04:11 +0200

    update sdk message list

--- util/scripts/generate-schema.sh
+++ util/scripts/generate-schema.sh
@@ -3,14 +3,14 @@ set -o errexit -o nounset -o pipefail
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-rm -rf "${SCRIPT_PATH}/schemas"
+#rm -rf "${SCRIPT_PATH}/schemas"
 # rm -rf "${SCRIPT_PATH}/Trustless-Hub"
 # git clone --depth 1 https://github.com/trstlabs/Trustless-Hub "${SCRIPT_PATH}/Trustless-Hub"
 
 
 
-INTO_DIR="${SCRIPT_PATH}/Trustless-Hub/proto"
-INTO_THIRD_PARTY_DIR="${SCRIPT_PATH}/Trustless-Hub/third_party/proto"
+INTO_DIR="${SCRIPT_PATH}/Trustless-Hub/third_party/proto"
+INTO_THIRD_PARTY_DIR="${SCRIPT_PATH}/Trustless-Hub/proto"
 
 
 # directory for individual schema files
@@ -18,40 +18,62 @@ MSGS_DIR="${SCRIPT_PATH}/schemas/msgs"
 
 INDEX_FILE="${MSGS_DIR}/index.ts"
 
-echo "${SCRIPT_PATH}"
-echo "${INTO_DIR}"
-echo "${INTO_THIRD_PARTY_DIR}"
-echo " $(find ${INTO_DIR} ${INTO_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)"
+# echo "${SCRIPT_PATH}"
+# echo "${INTO_DIR}"
+# echo "${INTO_THIRD_PARTY_DIR}"
+# echo " $(find ${INTO_DIR} ${INTO_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)"
 
 
 # Remove the msgs directory if it exists
-rm -rf "${MSGS_DIR}"
+# rm -rf "${MSGS_DIR}"
 # Recreate the msgs directory
-mkdir -p "${MSGS_DIR}"
+# mkdir -p "${MSGS_DIR}"
 
-# Generate individual JSON schema files for messages containing "Msg"
-find "${INTO_DIR}" "${INTO_THIRD_PARTY_DIR}" -path -prune -o -name '*.proto' -print0 | while IFS= read -r -d '' file; do
-    input_dir=$(dirname "${file}")
-    
-    # Create the output directory if it doesn't exist
-    mkdir -p "${MSGS_DIR}"
-    
-    # Check if the file contains a message with "Msg"
-    if grep -q 'message.Msg*' "${file}"; then
-        # Generate JSON schema file
-        protoc \
-        --plugin=$GOPATH/bin/protoc-gen-jsonschema \
-        --jsonschema_out=${MSGS_DIR} \
-        --jsonschema_opt=prefix_schema_files_with_package \
-        --jsonschema_opt=disallow_additional_properties \
-        --jsonschema_opt=enums_as_constants \
-        --jsonschema_opt=enums_as_strings_only \
-        --proto_path="${INTO_DIR}" \
-        --proto_path="${INTO_THIRD_PARTY_DIR}" \
-        "${file}"
-        
+# Remove old generated schemas
+# rm -rf schemas/msgs
+
+#Run buf generate
+cd ${SCRIPT_PATH}/ibc-go-main/proto
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template ${SCRIPT_PATH}/buf.gen.yaml "$file"
     fi
+  done
 done
+
+cd ${SCRIPT_PATH}/cosmos-sdk-main/proto
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template ${SCRIPT_PATH}/buf.gen.yaml "$file"
+    fi
+  done
+done
+
+
+cd ${SCRIPT_PATH}/wasmd-main/proto
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template ${SCRIPT_PATH}/buf.gen.yaml "$file"
+    fi
+  done
+done
+
+cd ${SCRIPT_PATH}/intento/proto
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template ${SCRIPT_PATH}/buf.gen.yaml "$file"
+    fi
+  done
+done
+
 
 # Loop through all subdirectories in MSGS_DIR
 for dir in "$MSGS_DIR"/*; do
@@ -80,19 +102,15 @@ for dir in "$MSGS_DIR"/*; do
     fi
 done
 
+SCHEMA_NAMES_FILE="${MSGS_DIR}/schemaNames.ts"
+echo "export const schemaNames = [" > "$SCHEMA_NAMES_FILE"
+
 # Iterate over all JSON files in the msgs directory
 for json_file in "${MSGS_DIR}"/*.json; do
-    # Check if the file exists
     if [ -f "$json_file" ]; then
-        # Get the file name without the extension
         file_name=$(basename "${json_file%.*}")
         
-        # Check if the file name starts with "Msg" and does not end with "Response"
         if [[ "$file_name" == *Msg* && "$file_name" != *Response ]]; then
-            
-            # Convert the keys in the "properties" section of all definitions to camelCase
-            # Set additionalProperties to true at the correct level
-            # Remove specific 'value' object
             updated_json=$(cat "$json_file" | jq '
             def snake_to_camel:
             gsub( "_(?<a>[a-z])"; .a|ascii_upcase);
@@ -117,23 +135,19 @@ for json_file in "${MSGS_DIR}"/*.json; do
             walk(add_additional_properties) |
             walk(if type == "object" and .value? and .value.type == "string" and .value.description == "Must be a valid serialized protocol buffer of the above specified type." and .value.format == "binary" and .value.binaryEncoding == "base64" then del(.value) else . end)
             ')
-            
-            # Overwrite the file with the modified JSON
+
             echo "$updated_json" > "${MSGS_DIR}"/"$file_name"+"_tmp".json
-            
-            # Remove the "$schema" key
             jq 'del(."$schema")' "${MSGS_DIR}"/"$file_name"+"_tmp".json > "$json_file"
-            
-            # Add an export statement to the index.ts file
-            echo "export { default as $file_name } from './$(basename "$json_file")';" >> "$INDEX_FILE"
             rm "${MSGS_DIR}"/"$file_name"+"_tmp".json
+
+            # Add to schemaNames list
+            echo "  \"$file_name\"," >> "$SCHEMA_NAMES_FILE"
         else
             rm "${MSGS_DIR}"/"$file_name".json
         fi
     fi
 done
 
+echo "] as const;" >> "$SCHEMA_NAMES_FILE"
 
-
-
-echo "Generated msg schemas and index.ts file in $MSGS_DIR"
+echo "Generated msg schemas and schemaNames.ts file in $MSGS_DIR"
