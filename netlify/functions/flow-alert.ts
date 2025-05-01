6dc5ffe472ad37469cfa3c196a4ab734047c216d
danieljdd <danieljdijkstra@gmail.com>
2025-05-01 12:15:21 +0200

    feat: alert for owner

--- netlify/functions/flow-alert.ts
+++ netlify/functions/flow-alert.ts
@@ -3,27 +3,28 @@ import * as Ably from 'ably'
 
 export const handler: Handler = async (event) => {
   try {
-    let email, flowID, unsubscribe
+    let email, flowID, owner, unsubscribe, type
 
     if (event.httpMethod === 'GET') {
-      // Extract query parameters from the URL
       const params = new URLSearchParams(event.queryStringParameters as any)
       email = params.get('email')
       flowID = params.get('flowID')
+      owner = params.get('owner')
       unsubscribe = params.get('unsubscribe') === 'true'
+      type = params.get('type') || 'all'
     } else {
-      // Handle POST request with JSON body
-      ({ email, flowID, unsubscribe } = JSON.parse(event.body || '{}'))
+      ({ email, flowID, owner, unsubscribe, type = 'all' } = JSON.parse(event.body || '{}'))
     }
 
-    if (!email || !flowID) {
-      console.warn('Invalid request: Missing email or flowID', event.body || event.queryStringParameters)
+    if (!email || (!flowID && !owner)) {
+      console.warn('Invalid request: Missing email or flowID/owner', event.body || event.queryStringParameters)
       return { statusCode: 400, body: 'Invalid request' }
     }
 
-    console.log(`Processing request for ${email} with flowID: ${flowID}, unsubscribe: ${unsubscribe}`)
+    const key = flowID ? 'flow:' + flowID : 'owner:' + owner
 
-    // Create Ably client
+    console.log(`Processing ${unsubscribe ? 'unsub' : 'sub'} request for ${email} on ${key} [type: ${type}]`)
+
     const ably = new Ably.Realtime({
       key: process.env.ABLY_API_KEY,
       clientId: email,
@@ -32,17 +33,17 @@ export const handler: Handler = async (event) => {
     const channel = ably.channels.get('flow-events')
     await channel.attach()
 
-    if (unsubscribe) {
-      // Publish the "unsubscribe" event to be processed by the worker
-      await channel.publish("unsubscribe", { email, flowID })
-      console.log(`Published 'unsubscribe' event for ${email} from flow ID ${flowID}`)
-      return { statusCode: 200, body: 'Unsubscribe request sent' }
-    }
+    const payload = flowID
+      ? { email, flowID, type }
+      : { email, owner, type }
 
-    // Publish the "subscribe" event for worker processing
-    await channel.publish("subscribe", { email, flowID })
-    console.log(`Published 'subscribe' event for ${email} to flow ID ${flowID}`)
-    return { statusCode: 200, body: 'Subscribe request sent' }
+    const action = unsubscribe ? 'unsubscribe' : 'subscribe'
+    await channel.publish(action, payload)
+
+    return {
+      statusCode: 200,
+      body: `${unsubscribe ? 'Unsubscribe' : 'Subscribe'} request sent`,
+    }
   } catch (error) {
     console.error('Error processing request:', error)
     return { statusCode: 500, body: 'Internal server error' }
